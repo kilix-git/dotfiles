ef425428ddf97d0f1da9d61edef5b2eda7f44d61
kilix-git <66571337+kilix-git@users.noreply.github.com>
2021-04-12 18:40:12 +0000

    Add files via upload

--- bin/spectrwm_baraction.sh
+++ bin/spectrwm_baraction.sh
@@ -1,47 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 # baraction.sh for spectrwm status bar
 
-## DISK
-hdd() {
-  hdd="$(df -h | awk 'NR==6{print $3,"- "$5}')"
-  echo -e "$hdd"
-}
+PKGS="$(pkg info | wc -l | sed -e 's/^[ \t]*//')"
+KERNEL="$(uname -v)"
 
-## RAM
-mem() {
-  mem=`free | awk '/Mem/ {printf "%dM\n", $3 / 1024.0}'`
-  echo -e "$mem "
-}
+echo "$KERNEL "   "|"   "   $PKGS   "   
+sleep 10	
 
-## CPU
-cpu() {
-  read cpu a b c previdle rest < /proc/stat
-  prevtotal=$((a+b+c+previdle))
-  sleep 0.5
-  read cpu a b c idle rest < /proc/stat
-  total=$((a+b+c+idle))
-  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-  temp=$(sensors | grep Package | awk '{print $4}')
-  echo -e "CPU: $cpu% "/" $temp"
-}
+while true; do
 
-## VOLUME
-vol() {
-    vol=`pacmd list-sinks | awk '/^\svolume:/{print $5; exit}'`
-    echo -e "VOL: $vol "
-}
-
-SLEEP_SEC=2
-#loops forever outputting a line every SLEEP_SEC secs
-
-# It seems that we are limited to how many characters can be displayed via
-# the baraction script output. And the the markup tags count in that limit.
-# So I would love to add more functions to this script but it makes the 
-# echo output too long to display correctly.
-while :; do
-    echo "+@fg=0; +@fn=1;+@fn=2; $(cpu) |" " +@fn=1;+@fn=2; $(mem)|" " +@fn=1;+@fn=2; $(hdd)|" " +@fn=1;  +@fn=2; $(vol)| "
-
-#    echo "$(vol) $(cpu) $(mem)"
-	sleep $SLEEP_SEC
+	KERNEL="$(uname -v | awk '{print $2}')"
+	UPTIME="$(uptime | awk -F, '{sub(".*up ",x,$1);print $1}' | sed -e 's/^[ \t]*//')"
+	VOL="$(mixer vol | awk -F ":" '{print $1}' | awk '{print $7}')"
+	MEM="$(freecolor -m -o | awk 'NR==2{print $3}')"
+	cpustat="$(ps -o %cpu -p `pgrep -S idle` | tail -1)"
+	CPU=`awk "BEGIN {print 400-$cpustat}"` 
+	CPUTEMP=`sysctl dev.cpu.2.temperature | awk '{print $2}'`
+	echo "  $VOL%  |    $MEM"M"  |   " "$CPU"% - "$CPUTEMP |    $UPTIME  "
+#  $KERNEL  |  
+	sleep 2
 done
-
+exit 0
